328ca3960d1e713fbf2f01c45a96c8c3dda998db
Steven <shuang07@icloud.ocm>
2016-02-08 01:51:24 +0000

    add mark as complete function

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
     before_action :set_todo_list
+     before_action :set_todo_item, except: [:create]
     
     
     def create
@@ -8,7 +9,6 @@ class TodoItemsController < ApplicationController
     end
     
     def destroy
-       @todo_item = @todo_list.todo_items.find(params[:id])
        if @todo_item.destroy
            flash[:success] = "Your item is deleted."
        else
@@ -17,7 +17,17 @@ class TodoItemsController < ApplicationController
        
        redirect_to @todo_list
     end
+    
+    def complete
+          @todo_item.update_attribute(:completed_at, Time.now)
+          redirect_to @todo_list, notice: "Todo item completed"
+    end
+    
+    
     private
+    def set_todo_item
+         @todo_item = @todo_list.todo_items.find(params[:id])
+    end
     
     def set_todo_list
         @todo_list = TodoList.find(params[:todo_list_id])
